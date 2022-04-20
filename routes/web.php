3b47eadb092bd2ddabc6a1915740f6c430a8b19b
ichadwiaprilia <ichadwiii05@gmail.com>
2022-04-20 12:45:41 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProfilController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nama', function () {
+    echo ('Icha Dwi BISA BISA');
+});
+
+Route::get('/nama/laravel', function () {
+    echo ('laravel');
+});
+
+Route::get('/profil', [ProfilController::class, 'index']);
