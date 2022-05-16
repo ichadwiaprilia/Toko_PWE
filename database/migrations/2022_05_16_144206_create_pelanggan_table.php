62206256b327e7297ff1c1e1c606807fb14035d4
ichadwiaprilia <ichadwiii05@gmail.com>
2022-05-16 21:50:42 +0700

    ERD Icha

---
+++ database/migrations/2022_05_16_144206_create_pelanggan_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePelangganTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nm_pel', 255);
+            $table->string('no_telp_pel', 16);
+            $table->string('almt_pel', 255);
+            $table->string('no_rek', 255);
+            $table->foreignId('admin_id')->constrained('admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelanggan');
+    }
+}
